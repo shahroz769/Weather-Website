5ef13d6b962af2043ae953e2b8fecd3096d05c0d
Shahroz Ahmed <shahroz.khan2124238@gmail.com>
2023-04-10 21:45:29 +0500

    revert some animations

--- app.js
+++ app.js
@@ -279,7 +279,6 @@ window.getSun = function () {
 }
 let splitWeather = new SplitType('.weather-app-split')
 gsap.from('.char', { y: '175px', stagger: 0.08, ease: "power4.inOut", duration: 1.5 })
-gsap.to(".card", {scale: 1,ease: "power4.out", duration: 1.5 }, "-=1.3")
-gsap.to(".card", { 'clip-path': 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)', opacity: 1, y: 0, ease: "power4.out", duration: 1.5 }, "-=1.3")
-gsap.fromTo("#doodle", { y: '-100px', opacity: 0 }, { y: 0, opacity: 1, ease: "power4.out", duration: 1.5 }, "-=1")
-gsap.fromTo(".search", { y: '-50px', opacity: 0 }, { y: 0, opacity: 1, ease: "power4.out", duration: 1.5 }, "-=1.5")
+gsap.to(".card", { 'clip-path': 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)', y: 0, ease: "power4.out", duration: 1.5 }, "-=1.3")
+gsap.fromTo("#doodle", { y: '-200px', opacity: 0 }, { y: 0, opacity: 1, ease: "power4.out", duration: 1.5 }, "-=1")
+gsap.fromTo(".search", { y: '-50px', opacity: 0 }, { y: 0, opacity: 1, ease: "power4.out", duration: 1.5 }, "-=1.3")
